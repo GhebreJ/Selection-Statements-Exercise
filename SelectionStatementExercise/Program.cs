198ed999b4772c7314e751e917470b8bad06e6bf
Jonah <jghebremichael95@gmail.com>
2023-01-02 18:34:44 -0500

    finished

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            var favNumber = 10;
+            var userInput = int.Parse(Console.ReadLine());
+
+            if(userInput < 10)
+            {
+                Console.WriteLine("too low");
+            }
+            else if(userInput > 10 )
+            {
+                Console.WriteLine("too high");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!!!!");
+            }
         }
     }
 }
